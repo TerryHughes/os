69847ffd272960a43c7521a39f4b220acb1fcaec
TerryH <terry@tnthughes.net>
2017-03-01 19:43:09 -0800

    Correct misspelled variable

--- build.bat
+++ build.bat
@@ -2,19 +2,19 @@
 SETLOCAL
 
 SET DisabledCompilerWarning=/wd4100
-SET CommomCompilerFlags=/D WINDOWSAPI /Z7 /nologo %DisabledCompilerWarning% /W4 /WX
+SET CommonCompilerFlags=/D WINDOWSAPI /Z7 /nologo %DisabledCompilerWarning% /W4 /WX
 SET CommonLinkerFlags=/WX
 
 IF "%1"=="release" GOTO release
 
 :debug
-SET CommonCompilerFlags=%CommomCompilerFlags% /D DEBUG
-SET CommonCompilerFlags=%CommomCompilerFlags% /D SLOW
+SET CommonCompilerFlags=%CommonCompilerFlags% /D DEBUG
+SET CommonCompilerFlags=%CommonCompilerFlags% /D SLOW
 SET CommonLinkerFlags=%CommomLinkerFlags% /DEBUG
 GOTO compile
 
 :release
-SET CommomCompilerFlags=%CommomCompilerFlags% /O2
+SET CommonCompilerFlags=%CommonCompilerFlags% /O2
 GOTO compile
 
 :compile
@@ -23,7 +23,7 @@ IF NOT EXIST build MKDIR build
 
 SET tool=assembler
 ctime -begin data\%tool%.ctm
-cl %CommomCompilerFlags% /Fe:build\%tool% /Fm:build\%tool% /Fo:build\%tool% toolchain\%tool%\platforms\WindowsAPI\main.c /link %CommonLinkerFlags%
+cl %CommonCompilerFlags% /Fe:build\%tool% /Fm:build\%tool% /Fo:build\%tool% toolchain\%tool%\platforms\WindowsAPI\main.c /link %CommonLinkerFlags%
 ctime -end data\%tool%.ctm %ERRORLEVEL%
 
 ctags --exclude=data --recurse *
